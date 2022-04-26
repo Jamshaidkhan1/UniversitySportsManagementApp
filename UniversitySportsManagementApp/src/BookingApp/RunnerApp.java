f147fad4086dbbd0c8cb58e55bda45873112a859
Dell <Dell@P2-Jamshid>
2022-04-27 01:53:26 +0430

    This is my first commit message of the University Sports Management App

---
+++ UniversitySportsManagementApp/src/BookingApp/RunnerApp.java
@@ -0,0 +1,13 @@
+package BookingApp;
+
+public class RunnerApp {
+	
+public static void main(String[] args) {
+		
+		System.out.println("1 : Please Select this option for Booking your exercise");
+		System.out.println("2 : Please use this option for chaning your bookings");
+		System.out.println("3 : Please Select this option for participating in lessons");
+		System.out.println("4 : Would you like to produce monthly Report for lesson?");
+		System.out.println("5 : Would you like to produce Monthly income Report?");
+
+}
